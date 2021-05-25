3896c94819997f08868bb88c5c53ec43c2f3f146
Guilherme Vieira <guilhermesv@gmail.com>
2021-05-25 13:16:53 -0300

    Novo compilador

--- Rotinas/Rotina_GifVideos.ps1
+++ Rotinas/Rotina_GifVideos.ps1
@@ -1,5 +1,7 @@
 magick convert *.png -set filename:base "%[basename]" "%[filename:base].jpg"
 
+Dir *.jpg | ForEach-Object  -begin { $count=1 }  -process { rename-item $_ -NewName "$count.jpg"; $count++ }
+
 magick convert -delay 20 -loop 0 +dither -colors 8 10.jpg 20.jpg 30.jpg 40.jpg 50.jpg 60.jpg 70.jpg 80.jpg 90.jpg 100.jpg thumb-b.gif
 
 gifsicle -O3 thumb-b.gif -o thumb.gif
